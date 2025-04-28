0b1d4866e6d84bc968f88315d57a64f0fa3293b4
Guilherme lima <66636474+GuiDev182@users.noreply.github.com>
2025-04-28 15:32:49 +0000

    1º dia do desafio de 100 dias de código - python
    aprendemos sobre:
    
     -- variaveis
     -- manipulção de string
     -- função input e print
     -- Nomes de variáveis

---
+++ day 1.py
@@ -0,0 +1,5 @@
+print("Welcome to the Band Generator")
+city = input("What's the name of the city you grew up in? \n R: ")
+pet_name = input("Whats your pet's name? \n R: ")
+band_name = city + ' ' +  pet_name           
+print(f"Your band name could be {band_name}")
