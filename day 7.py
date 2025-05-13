3d16c078687aeee878dcd0150b6c3252696255ea
GuiDev182 <geforcenow182@gmail.com>
2025-05-12 23:05:59 -0300

    Day 7 of 100 Conhecimento: step 3 in progress

--- day 7.py
+++ day 7.py
@@ -1,41 +1,50 @@
 #Step 1
-import random
-
-word_list = ["aardvark", "baboon", "camel"]
-
-
-#TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
-chosen_word = random.choice(word_list)
-print(chosen_word)
-#TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-guess = input("Guess a letter: \n R: ").lower()
-#TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word. print "Right" for correct
+#TODO-1 - Randomly choose a word from the word_list and assign it to a variable called palavra_forca.
+#TODO-2 - Ask the user to letra_escolhida a letter and assign their answer to a variable called letra_escolhida. Make letra_escolhida lowercase.
+#TODO-3 - Check if the letter the user letra_escolhidaed (letra_escolhida) is one of the letters in the palavra_forca. print "Right" for correct
 # and "wrong" if it's not
-for each_letter in chosen_word:
-    if guess == each_letter:
-        print("Right")
-    else:
-        print("Wrong")
-
 
 #Step 2
-
-
 #TODO-1: - Create an empty List called display.
-#For each letter in the chosen_word, add a "_" to 'display'.
-#So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to guess.
-display = []
+#For each letter in the palavra_forca, add a "_" to 'display'.
+#So if the palavra_forca was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to letra_escolhida.
+#TODO-2: - Loop through each position in the palavra_forca;
+#If the letter at that position matches 'letra_escolhida' then reveal that letter in the display at that position.
+#e.g. If the user letra_escolhidaed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
 
-#TODO-2: - Loop through each position in the chosen_word;
-#If the letter at that position matches 'guess' then reveal that letter in the display at that position.
-#e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
-for each_letter in chosen_word:
-    if guess == each_letter:
-        display.append(guess)
+#step 3
+#TODO-1: - Use a while loop to let the user letra_escolhida again. The loop should only stop once the user has letra_escolhidaed all the letters in the palavra_forca and 'display' has no more blanks ("_").
+# Then you can tell the user they've won.
+#TODO-2: Change the for loop so that you keep the previous correct letter
+
+
+import random
+
+lista_palavras = ["aardvark", "baboon", "camel"]
+
+
+palavra_forca= random.choice(lista_palavras)
+print(palavra_forca)
+lista_palavra_forca = []
+letra_escolhida = input("letra_escolhida a letter: \n R: ").lower()
+for cada_letra in palavra_forca:
+    if letra_escolhida == cada_letra:
+        lista_palavra_forca.append(letra_escolhida)
     else:
-        display.append('_')
-print(display)
+        lista_palavra_forca.append("_")
+contador_palavra = 0
+while contador_palavra != len(palavra_forca):
+    letra_escolhida = input("letra_escolhida a letter: \n R: ").lower()
+    for cada_letra in lista_palavra_forca:
+        if letra_escolhida == cada_letra:
+            lista_palavra_forca.remove('_')
+            lista_palavra_forca.append(letra_escolhida)
+        contador_palavra += 1
+    else:
+        lista_palavra_forca.append('_')
 
-#TODO-3: - Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
-#Hint - Don't worry about getting the user to guess the next letter. We'll tackle that in step 3.
+
+
+
+
 
