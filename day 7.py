993893ae9e649386a3d14c721654d2eeb526d2fb
GuiDev182 <geforcenow182@gmail.com>
2025-05-14 20:57:03 -0300

    Day 7 of 100 Conhecimento: projeto de jogo de forca usando loop for e while

--- day 7.py
+++ day 7.py
@@ -1,50 +1,107 @@
-#Step 1
-#TODO-1 - Randomly choose a word from the word_list and assign it to a variable called palavra_forca.
-#TODO-2 - Ask the user to letra_escolhida a letter and assign their answer to a variable called letra_escolhida. Make letra_escolhida lowercase.
-#TODO-3 - Check if the letter the user letra_escolhidaed (letra_escolhida) is one of the letters in the palavra_forca. print "Right" for correct
-# and "wrong" if it's not
-
-#Step 2
-#TODO-1: - Create an empty List called display.
-#For each letter in the palavra_forca, add a "_" to 'display'.
-#So if the palavra_forca was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to letra_escolhida.
-#TODO-2: - Loop through each position in the palavra_forca;
-#If the letter at that position matches 'letra_escolhida' then reveal that letter in the display at that position.
-#e.g. If the user letra_escolhidaed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
-
-#step 3
-#TODO-1: - Use a while loop to let the user letra_escolhida again. The loop should only stop once the user has letra_escolhidaed all the letters in the palavra_forca and 'display' has no more blanks ("_").
-# Then you can tell the user they've won.
-#TODO-2: Change the for loop so that you keep the previous correct letter
-
+#Step 4
 
 import random
 
-lista_palavras = ["aardvark", "baboon", "camel"]
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
 
+end_of_game = False
+word_list = ["ardvark", "baboon", "camel"]
+chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
 
-palavra_forca= random.choice(lista_palavras)
-print(palavra_forca)
-lista_palavra_forca = []
-letra_escolhida = input("letra_escolhida a letter: \n R: ").lower()
-for cada_letra in palavra_forca:
-    if letra_escolhida == cada_letra:
-        lista_palavra_forca.append(letra_escolhida)
-    else:
-        lista_palavra_forca.append("_")
-contador_palavra = 0
-while contador_palavra != len(palavra_forca):
-    letra_escolhida = input("letra_escolhida a letter: \n R: ").lower()
-    for cada_letra in lista_palavra_forca:
-        if letra_escolhida == cada_letra:
-            lista_palavra_forca.remove('_')
-            lista_palavra_forca.append(letra_escolhida)
-        contador_palavra += 1
-    else:
-        lista_palavra_forca.append('_')
+#TODO-1: - Create a variable called 'lives' to keep track of the number of lives left.
+#Set 'lives' to equal 6.
+lives = 6
 
+#Testing code
+print(f'Pssst, the solution is {chosen_word}.')
 
+#Create blanks
+display = []
+for _ in range(word_length):
+    display += "_"
 
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
 
+    #Check guessed letter
+    for position in range(word_length):
+        letter = chosen_word[position]
+       # print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+        if letter == guess:
+            display[position] = letter
 
+    #TODO-2: - If guess is not a letter in the chosen_word,
+    #Then reduce 'lives' by 1.
+    #If lives goes down to 0 then the game should stop and it should print "You lose."
+    if guess not in chosen_word:
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print("You lose.")
 
+    #Join all the elements in the list and turn it into a String.
+    print(f"{' '.join(display)}")
+
+    #Check if user has got all letters.
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
+
+    #TODO-3: - print the ASCII art from 'stages' that corresponds to the current number of 'lives' the user has remaining.
+    print(stages[lives])
